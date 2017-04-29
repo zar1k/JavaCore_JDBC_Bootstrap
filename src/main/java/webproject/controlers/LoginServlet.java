d44f1cb0a429bfb9bbe5ebeb630931336e4b829e
Andrew Zarazka <andreyzarazka@gmail.com>
2017-04-29 12:57:03 +0300

    Added bootstrap, jquery and font-awesome libraries

---
+++ src/main/java/webproject/controlers/LoginServlet.java
@@ -0,0 +1,21 @@
+package webproject.controlers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Andrew Zarazka on 29.04.2017.
+ */
+@WebServlet(name = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String login, password;
+
+        login = request.getParameter("login");
+        password = request.getParameter("password");
+    }
+}
---
+++ src/main/java/webproject/services/LoginService.java
@@ -0,0 +1,10 @@
+package webproject.services;
+
+/**
+ * Created by Andrew Zarazka on 29.04.2017.
+ */
+public class LoginService {
+    public boolean authenticate(String login, String password) {
+        return false;
+    }
+}
